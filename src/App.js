aee1c9b4e61f784ec517d9c48c33a088d97c7131
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-25 06:08:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from 'react';
+import WeatherApp from './WeatherApp';
+
+class App extends React.Component {
+  render(){
+  return (
+    <WeatherApp />
+  );
+  }
+}
+export default App;
